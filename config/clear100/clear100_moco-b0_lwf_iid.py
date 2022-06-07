e6cc759deb21d9d67827f2c31dca5e78249c0f1e
intern <intern@gpu0.elephant-arctic.ts.net>
2022-06-07 09:10:38 -0700

    add clear100 configs

--- config/clear100/clear100_moco-b0_lwf_iid.py
+++ config/clear100/clear100_moco-b0_lwf_iid.py
@@ -11,9 +11,12 @@ num_epochs = 200
 timestamp = 11
 alpha = np.linspace(0, 2, num=timestamp).tolist()
 
-name = 'clear100_moco-b0_lwf'
+name = 'clear100_moco-b0_lwf_iid'
 
-scenario = dict(feature_type='moco_b0')
+scenario = dict(
+    feature_type='moco_b0',
+    evaluation_protocol='iid',
+    seed=0)
 
 model = dict(
     head=dict(
@@ -30,6 +33,7 @@ cl_strategy = dict(
 
 work_dir = f'./work_dirs/{name}'
 loggers = [
+    dict(type='TensorboardLogger'),
     dict(type='TextLogger', file=f'{work_dir}/log.txt'),
     dict(type='InteractiveLogger'),
     dict(type='WandBLogger', project_name='avalanche', run_name=name)
